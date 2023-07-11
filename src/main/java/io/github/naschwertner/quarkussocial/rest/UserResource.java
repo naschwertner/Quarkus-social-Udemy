e7210ba5ed03900486e399fbf173cb02e3919ee2
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2023-07-10 22:47:08 -0300

    Adiciona delete e put

--- src/main/java/io/github/naschwertner/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/naschwertner/quarkussocial/rest/UserResource.java
@@ -34,4 +34,33 @@ public class UserResource {
         PanacheQuery<PanacheEntityBase> query = User.findAll();  //ctrl alt v --> para criar variavel
         return Response.ok(query.list()).build();
     }
+
+    @DELETE
+    @Path("/{id}") //quando foi realizado delete precisa ser passado id como parametro
+    //  /user/2
+    @Transactional
+    public Response deleteUser( @PathParam("id") Long id){
+        User user = User.findById(id);
+
+        //conferindo se o id passado como parametro existe, caso contrario retorna NOT FOUND
+        if (user != null) {
+            user.delete();
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response updateUser( @PathParam("id") Long id, CreateUserRequest userData){
+        User user = User.findById(id); //procurando usuario por id
+
+        if(user != null){ //se usuario diferente de nulo
+            user.setName(userData.getName()); //seta o nome passado como parametro no body requisição
+            user.setAge(userData.getAge()); //seta a idade passada como parametro no body da requisição
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build(); // se nao encontrar o id passado volta 404 not found
+
+    }
 }
